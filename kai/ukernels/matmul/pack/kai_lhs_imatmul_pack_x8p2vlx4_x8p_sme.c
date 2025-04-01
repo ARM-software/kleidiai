60209185a9fd8756450cfeab2db16afbbfab9d83
Emil Ohlsson <emil.ohlsson@arm.com>
2025-04-01 13:31:49 +0000

    Rename LHS imatmul pack kernel
    The kernel require the indirection pointers to be laid out in a packed
    manner, which need to be indicated by the input type name
    
    Signed-off-by: Emil Ohlsson <emil.ohlsson@arm.com>
    
    Approved-by: Felix Johnny Thomasmathibalan <felixjohnny.thomasmathibalan@arm.com>

--- kai/ukernels/matmul/pack/kai_lhs_imatmul_pack_x8p2vlx4_x8p_sme.c
+++ kai/ukernels/matmul/pack/kai_lhs_imatmul_pack_x8p2vlx4_x8p_sme.c
@@ -11,7 +11,7 @@
 #error This file must be compiled for AArch64, FEAT_SVE2.
 #else  // Architectural features check.
 
-#include "kai_lhs_imatmul_pack_x8p2vlx4_x8_sme.h"
+#include "kai_lhs_imatmul_pack_x8p2vlx4_x8p_sme.h"
 
 #include <stddef.h>
 #include <stdint.h>
@@ -22,33 +22,34 @@
 #define KR 4
 #define MAX_M_STEP (MR * (KAI_SME_VEC_LENGTH_MAX_BYTES / sizeof(int8_t)) / KR)
 
-static size_t kai_get_mr_lhs_imatmul_pack_x8p2vlx4_x8_sme(void) {
+static size_t kai_get_mr_lhs_imatmul_pack_x8p2vlx4_x8p_sme(void) {
     return MR * kai_get_sme_vector_length_u8() / KR;
 }
 
-size_t kai_get_m_step_lhs_imatmul_pack_x8p2vlx4_x8_sme(void) {
-    return kai_get_mr_lhs_imatmul_pack_x8p2vlx4_x8_sme();
+size_t kai_get_m_step_lhs_imatmul_pack_x8p2vlx4_x8p_sme(void) {
+    return kai_get_mr_lhs_imatmul_pack_x8p2vlx4_x8p_sme();
 }
 
-size_t kai_get_lhs_packed_offset_lhs_imatmul_pack_x8p2vlx4_x8_sme(
+size_t kai_get_lhs_packed_offset_lhs_imatmul_pack_x8p2vlx4_x8p_sme(
     size_t m_idx, size_t k_chunk_count, size_t k_chunk_length) {
-    KAI_ASSUME(m_idx % kai_get_m_step_lhs_imatmul_pack_x8p2vlx4_x8_sme() == 0);
+    KAI_ASSUME(m_idx % kai_get_m_step_lhs_imatmul_pack_x8p2vlx4_x8p_sme() == 0);
 
     return m_idx * k_chunk_count * kai_roundup(k_chunk_length, KR) * sizeof(int8_t);
 }
 
-size_t kai_get_lhs_packed_size_lhs_imatmul_pack_x8p2vlx4_x8_sme(size_t m, size_t k_chunk_count, size_t k_chunk_length) {
-    const size_t m_end = kai_roundup(m, kai_get_mr_lhs_imatmul_pack_x8p2vlx4_x8_sme());
-    return kai_get_lhs_packed_offset_lhs_imatmul_pack_x8p2vlx4_x8_sme(m_end, k_chunk_count, k_chunk_length);
+size_t kai_get_lhs_packed_size_lhs_imatmul_pack_x8p2vlx4_x8p_sme(
+    size_t m, size_t k_chunk_count, size_t k_chunk_length) {
+    const size_t m_end = kai_roundup(m, kai_get_mr_lhs_imatmul_pack_x8p2vlx4_x8p_sme());
+    return kai_get_lhs_packed_offset_lhs_imatmul_pack_x8p2vlx4_x8p_sme(m_end, k_chunk_count, k_chunk_length);
 }
 
-void kai_run_lhs_imatmul_pack_x8p2vlx4_x8_sme(
+void kai_run_lhs_imatmul_pack_x8p2vlx4_x8p_sme(
     size_t m, size_t k_chunk_count, size_t k_chunk_length, const void* const* lhs_ptrs, size_t lhs_ptr_offset,
     const void* zero, void* lhs_packed) {
     KAI_ASSUME(lhs_ptrs != NULL);
     KAI_ASSUME(lhs_packed != NULL);
 
-    const size_t m_step = kai_get_mr_lhs_imatmul_pack_x8p2vlx4_x8_sme();
+    const size_t m_step = kai_get_mr_lhs_imatmul_pack_x8p2vlx4_x8p_sme();
     const size_t row_offset = 0;
     const size_t width = k_chunk_length;
 
