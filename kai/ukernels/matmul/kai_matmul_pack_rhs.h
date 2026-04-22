c401e7158fa277eaf34a879b1676277b74eea089
Emil Ohlsson <emil.ohlsson@arm.com>
2026-04-22 11:47:25 +0000

    feat: Add RHS packing kernel for decomposable GEMM
    This change adds the x8 -> x8p4vsx4 RHS packing kernel
    
    Signed-off-by: Emil Ohlsson <emil.ohlsson@arm.com>
    
    Reviewed-by: Emil Ohlsson <emil.ohlsson@arm.com>
    Reviewed-by: Felix Johnny Thomasmathibalan <felixjohnny.thomasmathibalan@arm.com>
    Approved-by: Felix Johnny Thomasmathibalan <felixjohnny.thomasmathibalan@arm.com>

--- kai/ukernels/matmul/kai_matmul_pack_rhs.h
+++ kai/ukernels/matmul/kai_matmul_pack_rhs.h
@@ -73,6 +73,25 @@ struct kai_matmul_pack_rhs_uker_api kai_matmul_pack_rhs_nxk_x32p4vsx1bx32_x32_x3
 /// @return The micro-kernel API.
 struct kai_matmul_pack_rhs_uker_api kai_matmul_pack_rhs_nxk_x8p4vsx4_x8_sme(void);
 
+/// Non-transposed RHS packing micro-kernel for 8-bit data.
+///
+/// Required CPU features:
+///   * FEAT_SME
+///
+/// Configuration parameters: none.
+///
+/// Operands:
+///   * rhs_packed - The packed RHS matrix.
+///     * RHS matrix: 8-bit data in 4vsx4 blocked layout.
+///   * rhs - The RHS matrix.
+///     * RHS matrix: 8-bit data in KxN layout.
+///   * bias_n - Unused by this micro-kernel.
+///
+/// Supported flags: none.
+///
+/// @return The micro-kernel API.
+struct kai_matmul_pack_rhs_uker_api kai_matmul_pack_rhs_kxn_x8p4vsx4_x8_sme(void);
+
 #ifdef __cplusplus
 }  // extern "C"
 #endif
