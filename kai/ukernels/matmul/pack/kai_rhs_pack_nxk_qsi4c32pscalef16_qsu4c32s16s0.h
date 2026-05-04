b3c2497d725ce5039fa26bd0a8691ddbb5a73625
Dan Johansson <dan.johansson@arm.com>
2026-05-04 11:37:33 +0000

    fix: Fix include directive for size_t
    Signed-off-by: Dan Johansson <dan.johansson@arm.com>
    
    Approved-by: Emil Ohlsson <emil.ohlsson@arm.com>

--- kai/ukernels/matmul/pack/kai_rhs_pack_nxk_qsi4c32pscalef16_qsu4c32s16s0.h
+++ kai/ukernels/matmul/pack/kai_rhs_pack_nxk_qsi4c32pscalef16_qsu4c32s16s0.h
@@ -1,12 +1,12 @@
 //
-// SPDX-FileCopyrightText: Copyright 2024-2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+// SPDX-FileCopyrightText: Copyright 2024-2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
 //
 // SPDX-License-Identifier: Apache-2.0
 //
 #pragma once
 
+#include <stddef.h>
 #include <stdint.h>
-#include <stdlib.h>
 
 #include "kai/kai_common.h"
 
