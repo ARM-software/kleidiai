3fe306f1f56283d70b41ec69e1c40d450c7b4b2e
Emil Ohlsson <emil.ohlsson@arm.com>
2026-09-23 08:26:23 +0000

    fix: Address compiler warning
    There is a reported compiler warning on the `in` table. This change
    addresses that warning by using `enum` symbols instead.
    
    Signed-off-by: Emil Ohlsson <emil.ohlsson@arm.com>
    
    Approved-by: Anton Bondarenko <anton.bondarenko@arm.com>

--- kai/ukernels/matmul/pack/kai_lhs_pack_bf16p8x4_f16_neon.c
+++ kai/ukernels/matmul/pack/kai_lhs_pack_bf16p8x4_f16_neon.c
@@ -1,5 +1,5 @@
 //
-// SPDX-FileCopyrightText: Copyright 2024-2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+// SPDX-FileCopyrightText: Copyright 2024-2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
 //
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -19,58 +19,60 @@
 
 #include "kai/kai_common.h"
 
-static const size_t kai_mr = 8;
-static const size_t kai_kr = 4;
-static const size_t kai_sr = 1;
+enum {
+    MR = 8,
+    KR = 4,
+    SR = 1,
+};
 
 size_t kai_get_m_step_lhs_pack_bf16p8x4_f16_neon(size_t mr) {
-    KAI_ASSUME(mr == kai_mr);
+    KAI_ASSUME(mr == MR);
 
-    return kai_mr;
+    return MR;
 }
 
 size_t kai_get_lhs_offset_lhs_pack_bf16p8x4_f16_neon(size_t m_idx, size_t lhs_stride) {
-    KAI_ASSUME(m_idx % (kai_mr) == 0);
+    KAI_ASSUME(m_idx % (MR) == 0);
 
     return m_idx * lhs_stride;
 }
 
 size_t kai_get_lhs_packed_offset_lhs_pack_bf16p8x4_f16_neon(size_t m_idx, size_t k, size_t mr, size_t kr, size_t sr) {
-    KAI_ASSUME(m_idx % kai_mr == 0);
-    KAI_ASSUME(mr == kai_mr);
-    KAI_ASSUME(kr == kai_kr);
-    KAI_ASSUME(sr == kai_sr);
+    KAI_ASSUME(m_idx % MR == 0);
+    KAI_ASSUME(mr == MR);
+    KAI_ASSUME(kr == KR);
+    KAI_ASSUME(sr == SR);
 
-    return m_idx * kai_roundup(k, kai_kr) * sizeof(uint16_t);
+    return m_idx * kai_roundup(k, KR) * sizeof(uint16_t);
 }
 
 size_t kai_get_lhs_packed_size_lhs_pack_bf16p8x4_f16_neon(size_t m, size_t k, size_t mr, size_t kr, size_t sr) {
-    KAI_ASSUME(mr == kai_mr);
-    KAI_ASSUME(kr == kai_kr);
-    KAI_ASSUME(sr == kai_sr);
+    KAI_ASSUME(mr == MR);
+    KAI_ASSUME(kr == KR);
+    KAI_ASSUME(sr == SR);
 
-    return kai_roundup(m, kai_mr) * kai_roundup(k, kai_kr) * sizeof(uint16_t);
+    return kai_roundup(m, MR) * kai_roundup(k, KR) * sizeof(uint16_t);
 }
 
 void kai_run_lhs_pack_bf16p8x4_f16_neon(
     size_t m, size_t k, size_t mr, size_t kr, size_t sr, size_t m_idx_start, const void* lhs, size_t lhs_stride,
     void* lhs_packed) {
-    KAI_ASSUME(mr == kai_mr);
-    KAI_ASSUME(kr == kai_kr);
-    KAI_ASSUME(sr == kai_sr);
+    KAI_ASSUME(mr == MR);
+    KAI_ASSUME(kr == KR);
+    KAI_ASSUME(sr == SR);
     KAI_ASSUME(lhs != NULL);
     KAI_ASSUME(lhs_packed != NULL);
 
     KAI_ASSUME(m_idx_start == 0);
 
-    const size_t block_height = kai_mr;
+    const size_t block_height = MR;
     const size_t row_offset = 0;
 
-    const void* in[kai_mr];
+    const void* in[MR];
 
     for (size_t block_y = 0; block_y < m; block_y += block_height) {
         const size_t height = KAI_MIN(m - block_y, block_height);
-        void* out = (char*)lhs_packed + block_y * kai_roundup(k, kai_kr) * sizeof(uint16_t);
+        void* out = (char*)lhs_packed + block_y * kai_roundup(k, KR) * sizeof(uint16_t);
         size_t width = k;
 
         for (size_t y = 0; y < height; y++) {
